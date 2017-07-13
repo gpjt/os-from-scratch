655315f1f97eb3d8f4fe81e4b2ce4ffa965a7845
Giles Thomas <giles@pythonanywhere.com>
2017-07-13 20:58:03 +0100

    Further improvements to the hex-printing.  Reasonably happy with it now.

--- print-hex/print-hex.asm
+++ print-hex/print-hex.asm
@@ -14,61 +14,46 @@
 print_hex:
     pusha
 
-    mov bx, HEX_OUT
-    add bx, 5
+    mov bx, HEX_OUT     ; bx is where we store the address of the
+    add bx, 5           ; nybble we're working on
 
-    mov ax, dx
-    and ax, 0x000f
-    cmp ax, 10
-    jl digit1
-    add ax, 39
-digit1:
-    add ax, 48
-    mov [bx], al
-    sub bx, 1
+    mov cx, 4           ; four nybbles to print
 
-    shr dx, 4
-    mov ax, dx
-    and ax, 0x000f
-    cmp ax, 10
-    jl digit1
-    add ax, 39
-digit2:
-    add ax, 48
-    mov [bx], al
-    sub bx, 1
+hexloop:
+    cmp cx, 0           ; have we written them all yet?
+    je endhexloop       ; if so, we're done.
 
-    shr dx, 4
-    mov ax, dx
-    and ax, 0x000f
-    cmp ax, 10
-    jl digit3
-    add ax, 39
-digit3:
-    add ax, 48
-    mov [bx], al
-    sub bx, 1
+    mov ax, dx          ; get the rightmost nybble
+    and ax, 0x000f      ;
 
-    shr dx, 4
-    mov ax, dx
-    and ax, 0x000f
-    cmp ax, 10
-    jl digit4
-    add ax, 39
-digit4:
-    add ax, 48
-    mov [bx], al
-    sub bx, 1
+    cmp ax, 10          ; if it's a digit, then we need to add
+    jl is_digit         ; 48 to get the ASCII representation
+    add ax, 39          ; and if it's not we need to add
+is_digit:               ; 87 (as 'a' is 10, and character 97)
+    add ax, 48          ; 87 - 48 = 39
 
-    mov bx, HEX_OUT
-    call print_string
+    mov [bx], al        ; Store our ASCII in the template
+
+    sub bx, 1           ; On to the next byte in the template
+
+    shr dx, 4           ; Shift-right to the next nybble
+
+    sub cx, 1           ; On to the next round in the loop
+    jmp hexloop         ;
+
+endhexloop:
+
+    sub bx, 1           ; Ready to print (allow for the '0' -- the loop has already
+                        ; got us to the 'x')
+
+    call print_string   ; print it
 
     popa
     ret
 
 
 HEX_OUT:
-    db '0x0000' ,0
+    db '0x0000', 0
 
 
 %include "print_string.asm"
