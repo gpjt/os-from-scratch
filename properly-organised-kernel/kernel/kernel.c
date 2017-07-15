baba32091c025cc6509ce208be554df886bf3725
Giles Thomas <giles@giles.net>
2017-07-15 20:54:21 +0100

    New version with re-orginised code and a better Makefile

---
+++ properly-organised-kernel/kernel/kernel.c
@@ -0,0 +1,10 @@
+void foo() {
+}
+
+void main() {
+	// Create a char* pointing to the first text cell of video memory
+	char* video_memory = (char *) 0xb8000;
+	// Write an "X" -- NB no colour flags
+	*video_memory = 'X';
+        foo();
+}
