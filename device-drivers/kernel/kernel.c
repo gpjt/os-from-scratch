bd644f3768881f70a7d3bc28e41c660d7880771f
Giles Thomas <giles@giles.net>
2017-07-16 18:20:08 +0100

    A bit more testing

--- device-drivers/kernel/kernel.c
+++ device-drivers/kernel/kernel.c
@@ -2,12 +2,8 @@
 
 void main() {
 	clear_screen();
-	// print("Welcome to a trivial 32-bit OS\n");
-	// print_at("Center", 37, 13);
-	print_char('C', 37, 13, WHITE_ON_BLACK);
-	print_char('e', -1, -1, WHITE_ON_BLACK);
-	print_char('n', -1, -1, WHITE_ON_BLACK);
-	print_char('t', -1, -1, WHITE_ON_BLACK);
-	print_char('e', -1, -1, WHITE_ON_BLACK);
-	print_char('r', -1, -1, WHITE_ON_BLACK);
+	print("First line\n");
+	print("Second ");
+	print("line\n");
+	print_at("Center", 37, 13);
 }
